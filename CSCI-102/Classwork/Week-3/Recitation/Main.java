57dd34a8c13316ec155b5afd34f521cfa87b3836
Peter Peganov <peter@peganov.net>
2023-05-30 21:16:25 -0400

    added recitation exercise

---
+++ CSCI-102/Classwork/Week-3/Recitation/Main.java
@@ -0,0 +1,8 @@
+import recitation.MyArrayListClass;
+
+public class Main {
+    public static void main(String[] args){
+        MyArrayListClass<Integer> list = new MyArrayListClass<Integer>(10);
+        System.out.println(list.isEmpty());
+    }
+}
