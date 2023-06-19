c73432441b2713fb5e4d62d3f7371de55dd025a9
Peter Peganov <peter@peganov.net>
2023-06-18 23:32:56 -0400

    finished first project

--- CSCI-102/Projects/Assignement1/src/Part_A.java
+++ CSCI-102/Projects/Assignement1/src/Part_A.java
@@ -1,152 +1,164 @@
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-/*
- * Process is a class that represents a process in the OS.
- * It has a name and a list of resources it needs to run.
+/**
+ * This is the Process class. It contains a name and a list of resources.
  */
 class Process {
     String name;
-    List<String> resources;
+    LinkedList<String> resources;
 
-    /*
-     * Constructor for Process.
+    /**
+     * This is the constructor for the Process class.
+     *
+     * @param name      the name of the process
+     * @param resources the list of resources required by the process
      */
-    Process(String name, List<String> resources) {
+    Process(String name, LinkedList<String> resources) {
         this.name = name;
         this.resources = resources;
     }
 }
 
+/**
+ * This is the Part_A class.
+ */
 public class Part_A {
+
     /**
-     * main method to run the simulateOS method.
-     * @param args
+     * Main method.
+     *
+     * @param args Command line arguments.
      */
     public static void main(String[] args) {
 
-        /*
-         * We wrap the code in a try-catch block to catch any exceptions that may occur.
-         * We read the input file and store it in a string.
-         */
         String filePath = "test/TestCases.txt";
         String inputString = "";
         try {
-
             File inputFile = new File(filePath);
             Scanner scanner = new Scanner(inputFile);
             inputString = scanner.nextLine();
-            
-            /**
-             * While there are more lines in the file, we run the simulateOS method on the line and print the result.
-             */
+
             while (scanner.hasNextLine()) {
                 inputString = scanner.nextLine();
                 System.out.println(simulateOS(inputString));
             }
 
             scanner.close();
-            
+
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
             System.exit(1);
         }
-
     }
 
-    /** 
-     * simulateOS method takes in a string of processes and resources and returns the cycles it takes to run all the processes.
-     * @param inputString is a string of processes and resources.
-    */
+    /**
+     * This method simulates an operating system.
+     * It takes in a string of processes and their resources.
+     *
+     * @param inputString the input string representing processes and their resources
+     * @return the number of cycles required to complete all the processes
+     */
     static int simulateOS(String inputString) {
-        /**
-         * Processes are separated by a semicolon.
-         * We split the inputString by semicolon to get an array of processes.
-         */
         String[] processStrings = inputString.split(";");
-        List<Process> processes = new ArrayList<>();
+        LinkedList<Process> processes = new LinkedList<>();
 
-        /*
-         * cycles is the number of cycles it takes to run all the processes.
-         */
         int cycles = 0;
 
         /**
-         * We iterate through the array of processes and split each process by the open parenthesis.
-         * The first element of the array is the name of the process.
-         * The second element of the array is a comma separated list of resources the process needs.
-         * We create a new Process object with the name and resources and add it to the list of processes.
+         * This loop creates a list of processes.
          */
         for (String processString : processStrings) {
-
             String[] processInfo = processString.split("\\(");
             String name = processInfo[0];
             String[] resources = processInfo[1].split(",");
-            processes.add(new Process(name, Arrays.asList(resources)));
+            LinkedList<String> resourceList = new LinkedList<>();
 
+            // Add resources to the resource list
+            for (String resource : resources) {
+                resourceList.addLast(resource);
+            }
+
+            // Create a new process and add it to the list of processes
+            processes.addLast(new Process(name, resourceList));
         }
 
-        List<String> availableResources = new ArrayList<>();
-        List<Process> completedProcesses = new ArrayList<>();
+        LinkedList<String> availableResources = new LinkedList<>();
+        LinkedList<Process> completedProcesses = new LinkedList<>();
 
         /**
-         * We iterate through the list of processes until we have completed all the processes.
+         * This loop runs until all processes are completed.
          */
         while (completedProcesses.size() < processes.size()) {
-            /**
-             * We iterate through the list of processes.
-             * If the process is already completed, we skip it.
-             * If the process can run, we remove the resources it needs from the availableResources list and add it to the completedProcesses list.
-             */
-            for (Process process : processes) {
-                if (completedProcesses.contains(process)) {
+            for (int i = 0; i < processes.size(); i++) {
+                Process process = processes.get(i);
+
+                // Skip processes that have already been completed
+                if (completedProcesses.containsElement(process)) {
                     continue;
                 }
 
                 boolean canRun = true;
-                for (String resource : process.resources) {
-                    if (!availableResources.contains(resource)) {
+
+                // Check if the process can run by verifying if all required resources are available
+                for (int j = 0; j < process.resources.size(); j++) {
+                    String resource = process.resources.get(j);
+                    if (!availableResources.containsElement(resource)) {
                         canRun = false;
                         break;
                     }
                 }
 
                 if (canRun) {
-                    for (String resource : process.resources) {
+                    // Remove the resources required by the process from the available resources
+                    for (int j = 0; j < process.resources.size(); j++) {
+                        String resource = process.resources.get(j);
                         availableResources.remove(resource);
                     }
-                    completedProcesses.add(process);
+
+                    // Add the completed process to the list of completed processes
+                    completedProcesses.addLast(process);
                 }
             }
 
+            for (int i = 0; i < processes.size(); i++) {
+                Process process = processes.get(i);
 
-            for (Process process : processes) {
-                if (completedProcesses.contains(process)) {
+                // Skip processes that have already been completed
+                if (completedProcesses.containsElement(process)) {
                     continue;
                 }
 
-                for (String resource : process.resources) {
-                    if (!availableResources.contains(resource)) {
-                        availableResources.add(resource);
+                boolean hasAvailableResource = false;
+
+                // Check if there is at least one available resource for the process to run
+                for (int j = 0; j < process.resources.size(); j++) {
+                    String resource = process.resources.get(j);
+                    if (availableResources.containsElement(resource)) {
+                        hasAvailableResource = true;
+                        break;
                     }
+                }
+
+                if (hasAvailableResource) {
+                    continue;
+                }
+
+                // Add the resources required by the process to the available resources
+                for (int j = 0; j < process.resources.size(); j++) {
+                    String resource = process.resources.get(j);
+                    availableResources.addLast(resource);
                 }
             }
 
             /**
-             * We increment the cycles by 1.
+             * Increment the number of cycles.
              */
             cycles++;
         }
-
-        /**
-         * We return the number of cycles it took to run all the processes.
-         */
+        
         return cycles;
-
     }
+
 }
--- CSCI-102/Projects/Assignement1/src/Part_B.java
+++ CSCI-102/Projects/Assignement1/src/Part_B.java
@@ -1,132 +1,166 @@
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
-/*
- * Process is a class that represents a process in the OS.
- * It has a name and a list of resources it needs to run.
+/**
+ * This is the Part_B class.
  */
-class Process {
-    String name;
-    List<String> resources;
-
-    /*
-     * Constructor for Process.
-     */
-    Process(String name, List<String> resources) {
-        this.name = name;
-        this.resources = resources;
-    }
-}
-
 public class Part_B {
-    /* Define some constants that will be used throughout the program.
-    *  POSSIBLE_RESOURCES are the resources that a process can require to run.
-    *  MAX_CYCLES is the maximum number of cycles the simulation will run for.
-    *  RANDOM is a random number generator.
-    */
+    /**
+     * This is the Process class. It contains a name and a list of resources.
+     */
+    static class Process {
+        String name;
+        LinkedList<String> resources;
+
+        /**
+         * This is the constructor for the Process class.
+         *
+         * @param name      the name of the process
+         * @param resources the list of resources required by the process
+         */
+        Process(String name, LinkedList<String> resources) {
+            this.name = name;
+            this.resources = resources;
+        }
+    }
+
+    /**
+     * Here we define the needed constants.
+     */
     static final String[] POSSIBLE_RESOURCES = new String[]{"A", "B", "C"};
     static final int MAX_CYCLES = 1000;
     static final Random RANDOM = new Random();
 
     /**
-     * main method to run the simulateOS method.
-     * @param args
+     * Main method.
+     *
+     * @param args Command line arguments.
      */
     public static void main(String[] args) {
-        List<Process> processes = generateProcesses(20);
+        LinkedList<Process> processes = generateProcesses(20);
         System.out.println(simulateOS(processes));
     }
 
     /**
-     * generateProcesses method generates a list of processes with random names and resources.
-     * The number of processes generated is given by the count parameter.
-     * @param count
+     * This method generates a list of processes with random resources.
+     *
+     * @param count the number of processes to generate
      * @return a list of processes
      */
-    static List<Process> generateProcesses(int count) {
-        List<Process> processes = new ArrayList<>();
+    static LinkedList<Process> generateProcesses(int count) {
+        LinkedList<Process> processes = new LinkedList<>();
         for (int i = 0; i < count; i++) {
-            List<String> resources = new ArrayList<>();
-            int resourceCount = RANDOM.nextInt(3) + 1;
+            String name = "P" + i;
+            LinkedList<String> resources = new LinkedList<>();
+            int resourceCount = RANDOM.nextInt(POSSIBLE_RESOURCES.length);
             for (int j = 0; j < resourceCount; j++) {
-                resources.add(POSSIBLE_RESOURCES[RANDOM.nextInt(POSSIBLE_RESOURCES.length)]);
+                resources.addLast(POSSIBLE_RESOURCES[RANDOM.nextInt(POSSIBLE_RESOURCES.length)]);
             }
-            processes.add(new Process("P" + (i + 1), resources));
+            Process process = new Process(name, resources);
+            processes.addLast(process);
         }
         return processes;
     }
 
     /**
-     * simulateOS method takes in a list of processes and returns a message saying how many cycles it takes to run all the processes.
-     * @param processes
-     * @return a message saying how many cycles it takes to run all the processes
+     * This method simulates an operating system.
+     * It takes in a list of processes and their resources.
+     *
+     * @param processes the list of processes to simulate
+     * @return a message indicating the result of the simulation
      */
-    static String simulateOS(List<Process> processes) {
-        int cycles = 0;
-        List<String> availableResources = new ArrayList<>();
-        List<Process> completedProcesses = new ArrayList<>();
+    static String simulateOS(LinkedList<Process> processes) {
 
         /**
-         * We loop through the processes list until all processes are completed or the maximum number of cycles is reached.
+         * Here we define the needed variables.
          */
-        while (cycles < MAX_CYCLES && !processes.isEmpty()) {
-            /**
-             * We loop through the processes list and check if each process can run.
-             */
-            for (Process process : new ArrayList<>(processes)) {
-                if (canRun(process, availableResources)) {
-                    availableResources.removeAll(process.resources);
-                    processes.remove(process);
-                    completedProcesses.add(process);
+        int cycles = 0;
+        LinkedList<String> availableResources = new LinkedList<>();
+        LinkedList<Process> completedProcesses = new LinkedList<>();
+
+        /**
+         * Here we use a while loop to simulate the OS.
+         */
+        while (cycles < MAX_CYCLES && completedProcesses.size() < processes.size()) {
+            for (int i = 0; i < processes.size(); i++) {
+                Process process = processes.get(i);
+                if (completedProcesses.containsElement(process)) {
+                    continue;
+                }
+
+                /**
+                 * Here we check if the process can run.
+                 */
+                boolean canRun = true;
+                for (int j = 0; j < process.resources.size(); j++) {
+                    String resource = process.resources.get(j);
+                    if (!availableResources.containsElement(resource)) {
+                        canRun = false;
+                        break;
+                    }
+                }
+
+                /**
+                 * Here we run the process if it can run (using an if statement to check).
+                 */
+                if (canRun) {
+                    for (int j = 0; j < process.resources.size(); j++) {
+                        String resource = process.resources.get(j);
+                        availableResources.remove(resource);
+                    }
+                    completedProcesses.addLast(process);
                 }
             }
-            /*
-             * Print out the length of the processes list every 100 cycles.
+
+            for (int i = 0; i < processes.size(); i++) {
+                Process process = processes.get(i);
+                if (completedProcesses.containsElement(process)) {
+                    continue;
+                }
+
+                boolean hasAvailableResource = false;
+                for (int j = 0; j < process.resources.size(); j++) {
+                    String resource = process.resources.get(j);
+                    if (availableResources.containsElement(resource)) {
+                        hasAvailableResource = true;
+                        break;
+                    }
+                }
+
+                if (hasAvailableResource) {
+                    continue;
+                }
+
+                for (int j = 0; j < process.resources.size(); j++) {
+                    String resource = process.resources.get(j);
+                    availableResources.addLast(resource);
+                }
+            }
+
+            /**
+             * Here we increment the cycles.
+             */
+            cycles++;
+
+            /**
+             * Print the length of processes every 100th cycle to watch its growth.
              */
             if (cycles % 100 == 0) {
                 System.out.println("Length of processes at cycle " + cycles + ": " + processes.size());
             }
-            
-            /*
-             * Add two new processes and three new resources every cycle.
+
+            /**
+             * Generate new processes and add them to the existing list of processes.
              */
             processes.addAll(generateProcesses(2));
-            availableResources.addAll(Arrays.asList("A", "B", "C"));
-            
-            /*
-             * Increment the cycles variable.
-             */
-            cycles++;
         }
 
-        /*
-        * If the maximum number of cycles is reached, return a message saying how many processes are left.
-        * Otherwise, return a message saying how many cycles it took to complete all processes.
-        */
-
+        /**
+         * Here we return the result of the simulation.
+         */
         if (cycles == MAX_CYCLES) {
             return "After " + MAX_CYCLES + " cycles, " + processes.size() + " processes are left.";
         } else {
             return "All processes completed in " + cycles + " cycles.";
         }
-    }
-
-    /**
-     * canRun method takes in a process and a list of available resources and returns true if the process can run.
-     * Otherwise, it returns false.
-     * @param process
-     * @param availableResources
-     * @return
-     */
-    static boolean canRun(Process process, List<String> availableResources) {
-        for (String resource : process.resources) {
-            if (!availableResources.contains(resource)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
