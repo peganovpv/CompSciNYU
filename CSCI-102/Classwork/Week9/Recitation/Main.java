a1bbdf3f4546efc02a5474945ab540d2a30dd956
Peter Peganov <peter@peganov.net>
2023-07-25 15:32:05 -0400

    added selectionsort and bubblesort recitation

--- CSCI-102/Classwork/Week9/Recitation/Main.java
+++ CSCI-102/Classwork/Week9/Recitation/Main.java
@@ -1,15 +1,37 @@
+import java.util.Random;
+
 public class Main{
 
     public static void main(String[] args){
 
+        Random r = new Random();
+
         int[] arr = new int[1000];
+
+        for(int i = 0; i < 1000; i++){
+            arr[i] = r.nextInt(1000);
+        }
 
         int numComparisons_Bubble = BubbleSort(arr);
         System.out.println("Number of comparisons (Bubble Sort): " + numComparisons);
 
         int numComparisons_Selection = SelectionSort(arr);
         System.out.println("Number of comparisons (Selection Sort): " + numComparisons);
-        
+
+        // Array that favours Bubble Sort
+        int [] bubbleSortArray = {
+            1, 3, 2, 4, 5, 6, 7, 9, 8, 11, 10, 13, 12, 14, 16, 15, 17, 19, 18, 20
+        };
+        int numComparisons_Bubble_Favour = BubbleSort(bubbleSortArray);
+        System.out.println("Number of comparisons (Bubble Sort Favour Array): " + numComparisons_Bubble_Favour);
+
+        // Array that favours Selection Sort
+        int [] selectionSortArray = {
+            20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+        };
+        int numComparisons_Selection_Favour = SelectionSort(selectionSortArray);
+        System.out.println("Number of comparisons (Selection Sort Favour Array): " + numComparisons_Selection_Favour);
+
     }
 
     private static int BubbleSort(int[] arr){
